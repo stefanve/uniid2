3ae7f061f023dca89802b6e8668dff5b472ecc43
Stefan Vesterlund <stefan@poosty.com>
2017-07-17 12:34:41 +0200

    Initial commit

---
+++ shared/collections.js
@@ -0,0 +1,67 @@
+Schemas = {};
+var Collections = {};
+
+Merchants = Collections.Merchants = new Mongo.Collection("merchants");
+Customers = Collections.Customers = new Mongo.Collection("customers");
+
+
+Schemas.Address = new SimpleSchema({
+    street: {
+        type: String,
+        label: "Street ",
+        optional: true
+    },
+    house_number: {
+        type: String,
+        label: "House number",
+        optional: true
+    },
+    zip: {
+        type: String,
+        label: "ZIP code",
+        optional: true
+    },
+    city: {
+        type: String,
+        label: "City",
+        optional: true
+    },
+    country: {
+        type: String,
+        label: "Country",
+        optional: true
+    }
+
+});
+
+Schemas.Merchant = new SimpleSchema({
+    name: {
+        type: String,
+
+        optional: true
+    },
+    website_url:{
+        type: String,
+        label:  "Website",
+        optional: true
+    },
+    address: {
+        type: Schemas.Address
+    }
+
+});
+
+Schemas.Customer = new SimpleSchema({
+    name: {
+        type: String,
+
+        optional: true
+    },
+    address: {
+        type: Schemas.Address
+    }
+
+});
+
+Merchants.attachSchema(Schemas.Merchant);
+Customers.attachSchema(Schemas.Customer);
