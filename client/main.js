3b70feafe41eec9bb826d2b913675b80d6c18e86
Stefan Vesterlund <stefan@poosty.com>
2017-07-17 13:49:21 +0200

    babel

--- client/main.js
+++ client/main.js
@@ -2,7 +2,8 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-import '/imports/customer/client/add-customer.html';
+import '/imports/ui/customer/client/add-customer.html';
+import '/imports/ui/layouts/layouts.html';
 
 
 TAPi18n.setLanguage("de");
